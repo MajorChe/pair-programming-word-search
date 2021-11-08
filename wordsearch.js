e5f8a2a80b681a6d30724d75be7ba2bb6343a04c
Charit Nanduri <64259603+MajorChe@users.noreply.github.com>
2021-11-08 00:01:57 -0500

    Update wordsearch.js
    transpose the 2d array and see if the word is included in the array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const verticalArr = function(letters) {
+    let newArr = [];
+    for (let i = 0; i < letters[0].length; i++) { 
+      let miniArr = []
+      for (let j in letters) {
+        miniArr.push(letters[j][i]);
+      }
+      newArr.push(miniArr);
     }
+    return newArr;
+  };
+  const verticalJoin = verticalArr(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const horizontal = horizontalJoin.join('');
+  const vertical = verticalJoin.join('');
+  if (vertical.includes(word) || horizontal.includes(word)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
+
+
+
+
